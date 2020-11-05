3a7cb781e51050af6ca31f77d80bbccd7cf1fac8
miguepoloc <miguepoloc@gmail.com>
2020-11-05 08:40:36 -0500

    Primera integración de la plantilla con errores

---
+++ apps/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+# router.register(r'jugador', views.JugadorViewSet, "jugador")
+# router.register(r'children', views.ChildrenViewSet, "children")
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
